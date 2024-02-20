cf4461db122b483f92c25fe0d3c7dae0c2496461
Anthony E. <AnthonyDEpps@gmail.com>
2024-02-19 19:03:28 -0500

    feat: added floor component

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,17 +1,87 @@
-import styles from './table.module.css'
+// import styles from './table.module.css'
 
-//@ts-ignore
-export default function Table({id, isReserved, partyName, numSeats, order, onTableClick}: tableProps) {
-  // A small collection of elements which show the status and information about a given table.
-  // additionally, provides function to update current table status.
+// //@ts-ignore
+// export default function Table({id, isReserved, partyName, numSeats, order, onTableClick}: tableProps) {
+//   // A small collection of elements which show the status and information about a given table.
+//   // additionally, provides function to update current table status.
 
-  const availability = isReserved ? styles.tableNotAvailable : styles.tableAvailable;
-    return (
-      <div className={[styles.table, availability].join(' ')} onClick={onTableClick}>
-        <div className={styles.tDisplay}><img src="/final_table.png"></img></div>
-        <div className={styles.cDisplay}>🪑 x{numSeats}</div>
-        <button className={styles.statusButton}>{isReserved ? 'Reserved' : 'Open'}</button>
+//   const availability = isReserved ? styles.tableNotAvailable : styles.tableAvailable;
+//     return (
+//       <div className={[styles.table, availability].join(' ')} onClick={onTableClick}>
+//         <div className={styles.tDisplay}><img src="/final_table.png"></img></div>
+//         <div className={styles.cDisplay}>🪑 x{numSeats}</div>
+//         <button className={styles.statusButton}>{isReserved ? 'Reserved' : 'Open'}</button>
+//       </div>
+//     );
+//   }
+
+// import { useState } from "react";
+// import styles from "./table.module.css";
+
+// export default function Table(data: TableData) {
+//   console.log("id:", data.table_id);
+//   console.log("numSeats:", data.max_customer);
+//   console.log("isReserved?:", data.table_avaliable);
+//   const [isReserved, setReserved] = useState<boolean>(data.table_avaliable);
+
+//   const availability = isReserved
+//     ? styles.tableNotAvailable
+//     : styles.tableAvailable;
+
+//   const handleClick = () => {
+//     // Toggle the reservation state
+//     setReserved((prevReserved) => !prevReserved);
+
+//     // Execute the provided click handler
+//     // onTableClick();
+//   };
+
+//   return (
+//     <div
+//       className={[styles.table, availability].join(" ")}
+//       onClick={handleClick}
+//     >
+//       <div className={styles.tDisplay}>
+//         <img src="/final_table.png" alt="Table" />
+//       </div>
+//       <div className={styles.cDisplay}>🪑 x{data.max_customer}</div>
+//       <button className={styles.statusButton}>
+//         {isReserved ? "Reserved" : "Open"}
+//       </button>
+//     </div>
+//   );
+// }
+
+import { useState } from "react";
+import styles from "./table.module.css";
+
+export default function Table(data: TableData) {
+  const [available, setAvailable] = useState<boolean>(data.table_available);
+  console.log("Data:", data);
+  console.log("id:", data.table_id);
+  console.log("numSeats:", data.max_customer);
+  console.log("available?:", data.table_available);
+
+  const availability = data.table_available
+    ? styles.tableAvailable
+    : styles.tableNotAvailable;
+
+  const handleClick = () => {
+    setAvailable((prevAvailable) => !prevAvailable);
+  };
+
+  return (
+    <div
+      className={[styles.table, availability].join(" ")}
+      onClick={handleClick}
+    >
+      <div className={styles.tDisplay}>
+        <img src="/final_table.png" alt="Table" />
       </div>
-    );
-  }
-  
+      <div className={styles.cDisplay}>🪑 x{data.max_customer}</div>
+      <button className={styles.statusButton}>
+        {available ? "Reserved" : "Open"}
+      </button>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import TableTester from "@/components/table-tester";
 import Header from "@/components/header";
+import Floor from "@/components/floor";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,7 +10,8 @@ export default function Home() {
   return (
     <>
       <Header />
-      <TableTester numTables={10} />
+      {/* <TableTester numTables={10} /> */}
+      <Floor />
     </>
   ); // currently demonstrating Table component
 }
