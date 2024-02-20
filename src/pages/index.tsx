8749bd4f98e934771dfca038a17967b91307c364
Anthony E. <AnthonyDEpps@gmail.com>
2024-02-19 19:10:26 -0500

    fix: vercel error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import TableTester from "@/components/table-tester";
 import Header from "@/components/header";
 import Floor from "@/components/floor";
 
