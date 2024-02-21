b5e4bef50ab36b8d8435df2c89235471da68e21c
Anthony E. <AnthonyDEpps@gmail.com>
2024-02-20 21:58:32 -0500

    chore: added comments to explain refactor of table component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,11 @@
-import { Inter } from "next/font/google";
 import Header from "@/components/header";
 import Floor from "@/components/floor";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <Header />
-      {/* <TableTester numTables={10} /> */}
       <Floor />
     </>
-  ); /
+  );
 }
